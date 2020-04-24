fb2f4eb5a485c3036d5b22bfeb50a628d074316e
Blake Tarter <blakertarter@gmail.com>
2020-04-24 14:27:43 -0500

    Add example of some express middleware

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,33 @@
-import { ApolloServer } from 'apollo-server'
+import cors from "cors"
 import dotenv from 'dotenv'
-import { schema } from './schema'
-import { createContext } from './context'
-import { permissions } from "./permissions"
+import express from "express"
+import helmet from "helmet"
+import { ApolloServer } from 'apollo-server-express'
 import { applyMiddleware } from 'graphql-middleware'
 
+import { createContext } from './context'
+import { permissions } from "./permissions"
+import { schema } from './schema'
+
 dotenv.config()
+
+const APP_PORT = process.env.APP_PORT ?? 4000
+
+const app = express();
+
+app.use(helmet())
+app.use(cors())
 
 const server = new ApolloServer({
   schema: applyMiddleware(schema, permissions),
   context: createContext,
 })
 
-server.listen(
-  { port: 4000 },
-  () =>
-    console.log(
-      `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-apollo-server#using-the-graphql-api`,
-    ),
-)
+server.applyMiddleware({ app });
+
+app.listen({ port: APP_PORT }, () =>
+  console.log(
+    `🚀 Server ready at: http://localhost:${APP_PORT}${server.graphqlPath}\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-apollo-server#using-the-graphql-api`,
+  ),
+  
+);
