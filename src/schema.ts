9f2897a0abafb7292c6874c35edb9376a7904e2d
Blake Tarter <blakertarter@gmail.com>
2020-07-14 10:24:26 -0500

    Update nexus-schema

--- src/schema.ts
+++ src/schema.ts
@@ -197,7 +197,7 @@ const Mutation = objectType({
 
 export const schema = makeSchema({
   types: [Query, Mutation, AuthPayload, Post, Profile, User],
-  plugins: [nexusPrismaPlugin({ shouldGenerateArtifacts: true })],
+  plugins: [nexusPrismaPlugin({ shouldGenerateArtifacts: true, experimentalCRUD: true })],
   outputs: {
     schema: __dirname + '/../schema.graphql',
     typegen: __dirname + '/generated/nexus.ts',
